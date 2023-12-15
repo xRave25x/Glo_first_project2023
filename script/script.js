1bd9fd09ac2e24c052c6810c8e526643cdf2a638
Andrei Demichev <andyuson24@gmail.com>
2023-12-15 13:27:04 +0200

    чистый фаил для разработки

--- script/script.js
+++ script/script.js
@@ -1,12 +1,1 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
 
-
-alert('Hello Andy!');
-console.log('Hello script!');
-
-console.log("Узнали новые команды GIT");
