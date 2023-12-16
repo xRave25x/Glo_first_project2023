53bc6c94d72686ca86eff1307d5fa36c5fc48b46
Andrei Demichev <andyuson24@gmail.com>
2023-12-16 09:12:31 +0200

    Выполнили второй урок

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "first_project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 100;
+let rollback = 85;
+let fullPrice = 100000;
+let adaptive = true;
 
-alert("Hello!");
-console.log("Hello!");
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("стоимость верстки экранов " + screenPrice + " долларов" 
++ " и " + "стоимость разработки сайта " + fullPrice + " долларов" );
+console.log(screens.toLocaleLowerCase().split(","));
+console.log(fullPrice * (rollback / 100));
