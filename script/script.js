0b30160e35af16e02d9d9d17a9be3061bb240655
Andrei Demichev <andyuson24@gmail.com>
2023-12-18 20:11:11 +0200

    выполнил 3 урок

--- script/script.js
+++ script/script.js
@@ -1,10 +1,29 @@
+'use strict';
 
-const title = "first_project";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 100;
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 const rollback = 85;
-const fullPrice = 100000;
-const adaptive = true;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = fullPrice - rollback;
+console.log(Math.ceil(servicePercentPrice));
+
+
+if (fullPrice >= 30000){
+    console.log('Даем скидку 10%');
+}else if (fullPrice >= 15000 && fullPrice <= 30000){
+    console.log('Даем скидку 5%');
+}else if (fullPrice <= 15000 && fullPrice >= 0){
+    console.log('Скидка не предусмотрена');
+}else {
+    console.log('Что то пошло не так');
+} 
+
 
 
 console.log(typeof title);
@@ -15,3 +34,4 @@ console.log("стоимость верстки экранов " + screenPrice + 
 + " и " + "стоимость разработки сайта " + fullPrice + " долларов" );
 console.log(screens.toLocaleLowerCase().split(","));
 console.log(fullPrice * (rollback / 100));
+
